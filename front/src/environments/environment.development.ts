a0513cb1c6ab2068215133009d4d5c80616d2587
Debuzzz <nathan.fontaine53@gmail.com>
2026-03-11 06:59:16 +0100

    fix: change api key due to exposure

--- front/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  googleMapId: 'DEMO_MAP_ID',
-  googleApiKey: 'AIzaSyAIUMXDC3FUihnvQ7AsQurBKK5E0EI-fBM',
-};
---
+++ front/src/environments/environment.example.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  googleMapId: 'DEMO_MAP_ID',
+  googleApiKey: 'YOUR_GOOGLE_API_KEY',
+};
